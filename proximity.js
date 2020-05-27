ed539b3d14dcfc907d9035098017ba8d2effb38e
Vassili Philippov <vassili@melscience.com>
2020-05-27 02:51:26 +0100

    test 8 added

---
+++ proximity.js
@@ -0,0 +1,86 @@
+/**
+* The `Force` module [//todo: to be described].
+*
+* @class Force
+*/
+
+var Proximity = {};
+
+(function() {
+
+    Proximity.init = function(engine) {
+        Matter.Events.on(engine, "beforeUpdate", (event) => {
+            Proximity.check(engine, event.source.world.bodies);
+       });
+    };
+
+    //Todo: make on collision
+    //Todo: check each pair only once not twice
+    //Todo: for pair of chemistry particles make formula based events
+    Proximity.check = function(engine, bodies) {
+        for (bodyA of bodies) {
+            if (bodyA.plugin.proximity && bodyA.plugin.proximity.levels) {
+                if (!bodyA.plugin.proximity.levelFlags) {
+                    bodyA.plugin.proximity.levelFlags = {};
+                }
+                //Alias
+                let flags = bodyA.plugin.proximity.levelFlags; 
+                for (bodyB of bodies) {
+                    if (bodyA==bodyB) continue;
+                    let d = Proximity._distanceBetweenBodies(bodyA, bodyB);
+                    for (level of bodyA.plugin.proximity.levels) {
+                        if (!(level in flags)) {
+                            flags[level] = new Set();
+                        }
+                        if (d<level && !flags[level].has(bodyB)) {
+                            event = {bodyA: bodyA, bodyB: bodyB, level: level, distance: d}
+                            Matter.Events.trigger(bodyA, "comeClose", event);
+                            Matter.Events.trigger(engine, "comeClose", event);
+                            flags[level].add(bodyB);
+                        }
+                        if (d>level && flags[level].has(bodyB)) {
+                            event = {bodyA: bodyA, bodyB: bodyB, level: level, distance: d}
+                            Matter.Events.trigger(bodyA, "goAway", event);
+                            Matter.Events.trigger(engine, "goAway", event);
+                            flags[level].delete(bodyB);
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    Proximity._distanceBetweenAtoms = function(atomA, atomB) {
+        if (!atomA.circleRadius || !atomB.circleRadius) {
+            console.log("Error. Atoms without circleRadius property");
+            return Matter.Vector.magnitude(Matter.Vector.sub(atomA.position, atomB.position));
+        }
+        return Matter.Vector.magnitude(Matter.Vector.sub(atomA.position, atomB.position)) - atomA.circleRadius - atomB.circleRadius;
+    };
+
+    Proximity._distanceBetweenParticles = function(pA, pB) {
+        let minD = Infinity;
+        for (let k = pA.parts.length > 1 ? 1 : 0; k < pA.parts.length; k++) {
+            let partA = pA.parts[k];
+            for (let i = pB.parts.length > 1 ? 1 : 0; i < pB.parts.length; i++) {
+                let partB = pB.parts[i];
+                let d = Proximity._distanceBetweenAtoms(partA, partB);
+                if (d<minD) {
+                    minD = d;
+                }
+            }
+        }
+        return minD;
+    };
+
+    Proximity._distanceBetweenBodies = function(bodyA, bodyB) {
+        //Todo: replace with distance between any circle bodies
+        if (bodyA.plugin.chemistry && bodyA.plugin.chemistry.particle && bodyB.plugin.chemistry && bodyB.plugin.chemistry.particle) {
+            return Proximity._distanceBetweenParticles(bodyA, bodyB);
+        } else {
+             //Todo: Implement distance to other types of bodies like rectangle
+             return Matter.Vector.magnitude(Matter.Vector.sub(bodyA.position, bodyB.position));
+        }
+    };
+
+})();
