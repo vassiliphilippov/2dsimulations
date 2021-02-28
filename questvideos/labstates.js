bb78680693a1d7869778a0e5befca4294aa4ca03
Vassili Philippov <vassiliphilippov@yandex.ru>
2021-02-28 21:35:33 +0000

    aaa

--- questvideos/labstates.js
+++ questvideos/labstates.js
@@ -1,35 +1,291 @@
-materialChoice = {
-  [
-    {
-      
-    },
-  ]
+controlsBlue = {
+  acidSmall: {
+    picture: "acid-small.jpg", 
+    action: {newState: "blueGreen"}
+  }, 
+  acidMedium: {
+    picture: "acid-medium.jpg", 
+    action: {newState: "blueOrange"}
+  },	
+  acidBig: {
+    picture: "acid-big.jpg", 
+    action: {newState: "blueYellow"}
+  },	
+  acidVeryBig: {
+    picture: "acid-verybig.jpg", 
+    action: {newState: "blueRed"}
+  },	
+  baseSmall: {
+    picture: "base-small.jpg", 
+    action: {newState: "blueBlue"}
+  },	
+  baseMedium: {
+    picture: "base-medium.jpg", 
+    action: {newState: "blueBlue"}
+  },	
+  baseBig: {
+    picture: "base-big.jpg", 
+    action: {newState: "blueBlue"}
+  },	
+  baseVeryBig: {
+    picture: "base-verybig.jpg", 
+    action: {newState: "blueBlue"}
+  }
 };
 
-heatingLabStates = {
-  idle: {
-    name: "Idle",
-    videoFile: "idle.mp4",
-    pictures: {
-      material: "material_choose.jpg",
-      temperature: "temperature_room.jpg"
-    },
-    pictureClicks: {
-      material: materialChoice,
-      temperature: temperatureChoice
-    }  
+controlsGreen = {
+  acidSmall: {
+    picture: "acid-small.jpg", 
+    action: {newState: "greenYellow"}
+  }, 
+  acidMedium: {
+    picture: "acid-medium.jpg", 
+    action: {newState: "greenOrange"}
+  },	
+  acidBig: {
+    picture: "acid-big.jpg", 
+    action: {newState: "greenRed"}
+  },	
+  acidVeryBig: {
+    picture: "acid-verybig.jpg", 
+    action: {newState: "greenRed"}
+  },	
+  baseSmall: {
+    picture: "base-small.jpg", 
+    action: {newState: "greenBlue"}
+  },	
+  baseMedium: {
+    picture: "base-medium.jpg", 
+    action: {newState: "greenBlue"}
+  },	
+  baseBig: {
+    picture: "base-big.jpg", 
+    action: {newState: "greenBlue"}
+  },	
+  baseVeryBig: {
+    picture: "base-verybig.jpg", 
+    action: {newState: "greenBlue"}
+  }
+};
+
+controlsOrange = {
+  acidSmall: {
+    picture: "acid-small.jpg", 
+    action: {newState: "orangeRed"}
+  }, 
+  acidMedium: {
+    picture: "acid-medium.jpg", 
+    action: {newState: "orangeRed"}
+  },	
+  acidBig: {
+    picture: "acid-big.jpg", 
+    action: {newState: "orangeRed"}
+  },	
+  acidVeryBig: {
+    picture: "acid-verybig.jpg", 
+    action: {newState: "orangeRed"}
+  },	
+  baseSmall: {
+    picture: "base-small.jpg", 
+    action: {newState: "orangeYellow"}
+  },	
+  baseMedium: {
+    picture: "base-medium.jpg", 
+    action: {newState: "orangeGreen"}
+  },	
+  baseBig: {
+    picture: "base-big.jpg", 
+    action: {newState: "orangeBlue"}
+  },	
+  baseVeryBig: {
+    picture: "base-verybig.jpg", 
+    action: {newState: "orangeBlue"}
+  }
+};
+
+controlsYellow = {
+  acidSmall: {
+    picture: "acid-small.jpg", 
+    action: {newState: "yellowOrange"}
+  }, 
+  acidMedium: {
+    picture: "acid-medium.jpg", 
+    action: {newState: "yellowRed"}
+  },	
+  acidBig: {
+    picture: "acid-big.jpg", 
+    action: {newState: "yellowRed"}
+  },	
+  acidVeryBig: {
+    picture: "acid-verybig.jpg", 
+    action: {newState: "yellowRed"}
+  },	
+  baseSmall: {
+    picture: "base-small.jpg", 
+    action: {newState: "yellowGreen"}
+  },	
+  baseMedium: {
+    picture: "base-medium.jpg", 
+    action: {newState: "yellowBlue"}
+  },	
+  baseBig: {
+    picture: "base-big.jpg", 
+    action: {newState: "yellowBlue"}
+  },	
+  baseVeryBig: {
+    picture: "base-verybig.jpg", 
+    action: {newState: "yellowBlue"}
+  }
+};
+
+controlsRed = {
+  acidSmall: {
+    picture: "acid-small.jpg", 
+    action: {newState: "redRed"}
+  }, 
+  acidMedium: {
+    picture: "acid-medium.jpg", 
+    action: {newState: "redRed"}
+  },	
+  acidBig: {
+    picture: "acid-big.jpg", 
+    action: {newState: "redRed"}
+  },	
+  acidVeryBig: {
+    picture: "acid-verybig.jpg", 
+    action: {newState: "redRed"}
+  },	
+  baseSmall: {
+    picture: "base-small.jpg", 
+    action: {newState: "redYellow"}
+  },	
+  baseMedium: {
+    picture: "base-medium.jpg", 
+    action: {newState: "redOrange"}
+  },	
+  baseBig: {
+    picture: "base-big.jpg", 
+    action: {newState: "redGreen"}
+  },	
+  baseVeryBig: {
+    picture: "base-verybig.jpg", 
+    action: {newState: "redBlue"}
+  }
+};
+
+phLabStates = {
+  blueBlue: {
+    videoFile: "blue-blue.mp4", 
+    controls: controlsBlue
   },
 
-  iron20: {
-    name: "Iron 20°C",
-    videoFile: "iron20.mp4",
-    pictures: {
-      material: "material_iron.jpg",
-      temperature: "temperature_20.jpg"
-    },
-    pictureClicks: {
-      material: materialChoice,
-      temperature: temperatureChoice
-    }  
+  blueGreen: {
+    videoFile: "blue-green.mp4", 
+    controls: controlsGreen  
   },
+
+  blueYellow: {
+    videoFile: "blue-yellow.mp4", 
+    controls: controlsYellow  
+  },
+
+  blueOrange: {
+    videoFile: "blue-orange.mp4", 
+    controls: controlsOrange  
+  },
+
+  blueRed: {
+    videoFile: "blue-red.mp4", 
+    controls: controlsRed  
+  },
+
+  greenBlue: {
+    videoFile: "green-blue.mp4", 
+    controls: controlsBlue  
+  },
+
+  greenYellow: {
+    videoFile: "green-yellow.mp4", 
+    controls: controlsYellow  
+  },
+
+  greenOrange: {
+    videoFile: "green-orange.mp4", 
+    controls: controlsOrange  
+  },
+
+  greenRed: {
+    videoFile: "green-red.mp4", 
+    controls: controlsRed  
+  },
+
+  yellowBlue: {
+    videoFile: "yellow-blue.mp4", 
+    controls: controlsBlue  
+  },
+
+  yellowGreen: {
+    videoFile: "yellow-green.mp4", 
+    controls: controlsGreen  
+  },
+
+  yellow: {
+    videoFile: "yellow.mp4", 
+    controls: controlsYellow  
+  },
+
+  yellowOrange: {
+    videoFile: "yellow-orange.mp4", 
+    controls: controlsOrange  
+  },
+
+  yellowRed: {
+    videoFile: "yellow-red.mp4", 
+    controls: controlsRed  
+  },
+
+  orangeBlue: {
+    videoFile: "orange-blue.mp4", 
+    controls: controlsBlue  
+  },
+
+  orangeGreen: {
+    videoFile: "orange-green.mp4", 
+    controls: controlsGreen  
+  },
+
+  orangeYellow: {
+    videoFile: "orange-yellow.mp4", 
+    controls: controlsYellow  
+  },
+
+  orangeRed: {
+    videoFile: "orange-red.mp4", 
+    controls: controlsRed  
+  },
+
+  redBlue: {
+    videoFile: "red-blue.mp4", 
+    controls: controlsBlue  
+  },
+
+  redGreen: {
+    videoFile: "red-green.mp4", 
+    controls: controlsGreen  
+  },
+
+  redYellow: {
+    videoFile: "red-yellow.mp4", 
+    controls: controlsYellow  
+  },
+
+  redOrange: {
+    videoFile: "red-orange.mp4", 
+    controls: controlsOrange  
+  },
+
+  redRed: {
+    videoFile: "red-red.mp4", 
+    controls: controlsRed  
+  }
 };
