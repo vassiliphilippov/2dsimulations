469249c12f4cac3f7cf76d620771ce5b447b9cd8
Vassili Philippov <vassili@melscience.com>
2021-05-27 11:50:35 +0100

    quiz5

---
+++ data/crystalstructures.js
@@ -0,0 +1,89 @@
+var crystalStructures = [
+    {
+        formula: "CuSO4",
+        places : {
+            1: {formula: "Cu+2", position: {x: 140, y: 140}, angle: 0, charge: 2},
+            2: {formula: "SO4-2", position: {x: 403, y: 403}, angle: 0, charge: -2}
+        },
+        elementaryCellSize: {x: 526, y: 526}
+    },
+
+    {
+        formula: "FeSO4",
+        places : {
+            1: {formula: "Fe+2", position: {x: 140, y: 140}, angle: 0, charge: 2},
+            2: {formula: "SO4-2", position: {x: 403, y: 403}, angle: 0, charge: -2}
+        },
+        elementaryCellSize: {x: 526, y: 526}
+    },
+
+    {
+        formula: "MnSO4",
+        places : {
+            1: {formula: "Mn+2", position: {x: 140, y: 140}, angle: 0, charge: 2},
+            2: {formula: "SO4-2", position: {x: 403, y: 403}, angle: 0, charge: -2}
+        },
+        elementaryCellSize: {x: 526, y: 526}
+    },
+
+    {
+        formula: "CaSO4",
+        places : {
+            1: {formula: "Ca+2", position: {x: 140, y: 140}, angle: 0, charge: 2},
+            2: {formula: "SO4-2", position: {x: 403, y: 403}, angle: 0, charge: -2}
+        },
+        elementaryCellSize: {x: 526, y: 526}
+    },
+
+    {
+        formula: "NaCl",
+        places: {
+            1: {formula: "Na+", position: {x: 170, y: 170}, angle: 0, charge: 1},
+            2: {formula: "Cl-", position: {x: 490, y: 170}, angle: 0, charge: -1},
+            3: {formula: "Cl-", position: {x: 170, y: 490}, angle: 0, charge: -1},
+            4: {formula: "Na+", position: {x: 490, y: 490}, angle: 0, charge: 1}
+        },
+        elementaryCellSize: {x: 645, y: 645}
+    },
+
+    {
+        formula: "SnCl2",
+        places: {
+            1: {formula: "Sn+2", position: {x: 145, y: 424}, angle: 0, charge: 2},
+            2: {formula: "Cl-", position: {x: 145, y: 155}, angle: 0, charge: -1},
+            3: {formula: "Cl-", position: {x: 145, y: 769}, angle: 0, charge: -1},
+            4: {formula: "Sn+2", position: {x: 604, y: 424}, angle: 0, charge: 2},
+            5: {formula: "Cl-", position: {x: 604, y: 696}, angle: 0, charge: -1},
+            6: {formula: "Cl-", position: {x: 604, y: 113}, angle: 0, charge: -1}
+        },
+        elementaryCellSize: {x: 925, y: 894}
+    }
+
+]
+
+
+/*
+NaHSO4
+NaHCO3
+Na2CO3
+Citric acid
+Urea
+K3[Fe(CN)6]
+K4[Fe(CN)6] 
+FeS2
+Na2S2O5
+Na2S2O3
+NaCl
+(NH4)2CO3
+(NH4)Fe(SO4)2
+NH4Cl
+KI
+FeCl3
+MnO2
+Ca(OH)2
+(CuOH)2CO3
+Ca(NO3)2
+Graphite
+NaOH
+CaO
+*/
